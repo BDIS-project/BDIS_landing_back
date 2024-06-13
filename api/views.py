b0775967e0563c7fb4719e6096d05d3c32a29b42
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2024-06-13 13:43:28 +0300

    Modified query and database

--- api/views.py
+++ api/views.py
@@ -12,13 +12,22 @@ class StoreProductsAPIView(APIView):
 
     def get(self, request, *args, **kwargs):
         with connection.cursor() as cursor:
-            cursor.execute("SELECT Store_Product.id_product, product_name, selling_price, products_number, promotional_product "+
-                           "FROM Store_Product INNER JOIN Product ON Store_Product.id_product = Product.id_product "+
-                           "ORDER BY products_number;")
+            cursor.execute(
+                "SELECT Store_Product.id_product, product_name, selling_price, products_number, promotional_product, category_name "
+                "FROM Store_Product INNER JOIN Product ON Store_Product.id_product = Product.id_product "
+                "INNER JOIN Category ON Product.category_number = Category.category_number "
+                "ORDER BY products_number;"
+            )
             rows = cursor.fetchall()
             columns = [col[0] for col in cursor.description]
 
-        result = [dict(zip(columns, row)) for row in rows]
-        print(result)
+        result = []
+        for row in rows:
+            product = dict(zip(columns, row))
+            if product['promotional_product']:
+                product['original_price'] = round(float(product['selling_price']) / 0.8, 2)
+            product['selling_price'] = round(float(product['selling_price']), 2)
+            result.append(product)
 
+        print(result)
         return Response(result)
