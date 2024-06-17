8384e12dc4a6a228e0958996c7735a221e22ae02
Andrii Negrub <zurg0505@gmail.com>
2024-06-17 15:58:19 +0300

    auth test

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from . import views # for views.get_schedule?
-from .views import * # for ScheduleAPIView?
+from .views import * 
 from rest_framework_simplejwt import views as jwt_views
 
 from django.contrib import admin
@@ -9,4 +8,6 @@ from django.urls import path, include
 urlpatterns = [
     path('products', StoreProducts2APIView.as_view(), name='store-products'),
     path('categories', CategoriesAPIView.as_view(), name='get_categories'),
+    path('api/login/', LoginView.as_view(), name='login'),
+    path('api/logout/', LogoutView.as_view(), name='logout'),
 ]
