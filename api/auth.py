cca6de5e487c528f6e4ffd1f26cc2ced5dbafb25
Andrii Negrub <zurg0505@gmail.com>
2024-06-18 19:34:32 +0300

    quickfix #1 (still does not work)

--- api/auth.py
+++ api/auth.py
@@ -1,10 +1,25 @@
 from rest_framework_simplejwt.authentication import JWTAuthentication as BaseJWTAuthentication
-from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework_simplejwt.exceptions import InvalidToken, AuthenticationFailed
 from django.db import connection
 
 class JWTAuthentication(BaseJWTAuthentication):
+    def get_raw_token(self, request):
+        auth_header = request.headers.get('Authorization', None)
+        if not auth_header:
+            raise AuthenticationFailed('Authorization header not found')
+
+        parts = auth_header.split()
+        if parts[0].lower() != 'Bearer':
+            raise AuthenticationFailed('Invalid token header')
+        if len(parts) == 1:
+            raise AuthenticationFailed('Token not found')
+        elif len(parts) > 2:
+            raise AuthenticationFailed('Invalid token header')
+
+        return parts[1]
+
     def get_user(self, validated_token):
-        user_id = validated_token['user_id']
+        user_id = validated_token['user_id']  # Adjust this according to your token structure
 
         # Query your database to retrieve the user based on user_id
         with connection.cursor() as cursor:
@@ -23,18 +38,12 @@ class JWTAuthentication(BaseJWTAuthentication):
                 'id': user_id,
                 'username': username,
                 'role': role,
-                'is_authenticated': True
-
             }
         else:
             raise InvalidToken('User not found or token is invalid.')
 
     def authenticate(self, request):
-        """
-        Authenticate the request and return a two-tuple of (user, validated_token).
-        """
         raw_token = self.get_raw_token(request)
         validated_token = self.get_validated_token(raw_token)
         user = self.get_user(validated_token)
         return user, validated_token
-
