7d47a96ebbee4bcad77bdab5341f9360d7b5b962
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2024-06-20 09:53:04 +0300

    Added APIs for report queries

--- api/urls.py
+++ api/urls.py
@@ -7,32 +7,47 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+
+    # urls for updating
+    path('categories/<int:category_number>/updateCategory/', CategoriesAPIView.as_view(), name='update_category'), 
+    path('products/<int:id_product>/updateProduct/', ProductsAPIView.as_view(), name='update_product'), 
+    path('store-products/<str:UPC>/updateStoreProduct/', StoreProductsAPIView.as_view(), name='update_store_product'),
+    
     # urls for authentification
-    path('api/login/', LoginView.as_view(), name='login'),
-    path('api/logout/', LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     # urls for DQL-queries
-    path('products', ProductsAPIView.as_view(), name = 'products'), # CASHIER
-    path('store-products', StoreProductsAPIView.as_view(), name='store-products'), # CASHIER
-    path('check-overview', CheckOverviewAPIView.as_view(), name = 'check-overview'), # CASHIER
-    path('categories', CategoriesAPIView.as_view(), name='get_categories'), # CASHIER, DROPDOWN LIST
-    path('product-names', ProductNamesAPIView.as_view(), name = 'product-names'), # DROPDOWN LIST
-    path('store-overview/', StoreOverviewAPIView.as_view(), name = 'store-overview'), # MANAGER
-    path('about-me', AboutMeAPIView.as_view(), name = 'about-me'), # CASHIER
-    path('/reports', ReportsAPIView.as_view(), name = 'reports'), # MANAGER
+    path('products/', ProductsAPIView.as_view(), name = 'products'), # CASHIER
+    path('store-products/', StoreProductsAPIView.as_view(), name='store-products'), # CASHIER
+    path('check-overview/', CheckOverviewAPIView.as_view(), name='check-overview'), # CASHIER
+    path('categories/', CategoriesAPIView.as_view(), name='get_categories'), # CASHIER, DROPDOWN LIST
+    path('product-names/', ProductNamesAPIView.as_view(), name='product-names'), # DROPDOWN LIST
+    path('store-overview/', StoreOverviewAPIView.as_view(), name='store-overview'), # MANAGER
+    path('about-me/', AboutMeAPIView.as_view(), name='about-me'), # CASHIER
+    path('reports/', ReportsAPIView.as_view(), name='reports'), # MANAGER
 
     # urls for entities creation for MANAGER
-    path('create-category', CreateCategoryAPIView.as_view(), name = 'create-category'),
-    path('create-product', CreateProductAPIView.as_view(), name = 'create-product'),
-    path('create-store-product', CreateStoreProductAPIView.as_view(), name = 'create-store-product'),
-    path('create-employee', CreateEmployeeAPIView.as_view(), name = 'create-employee'),
-    path('create-customer', CreateCustomerAPIView.as_view(), name = 'create-customer'), # and CASHIER
+    path('create-category/', CreateCategoryAPIView.as_view(), name='create-category'),
+    path('create-product/', CreateProductAPIView.as_view(), name='create-product'),
+    path('create-store-product/', CreateStoreProductAPIView.as_view(), name='create-store-product'),
+    path('create-employee/', CreateEmployeeAPIView.as_view(), name='create-employee'),
+    path('create-customer/', CreateCustomerAPIView.as_view(), name='create-customer'), # and CASHIER
 
     # urls for entities deletion for MANAGER
     path('delete-category/<int:category_number>/', DeleteCategoryAPIView.as_view(), name='delete-category'),
-    path('delete-product/<int:id_product>/', DeleteProductAPIView.as_view(), name = 'delete-product'),
-    path('delete-store-product/<str:UPC>/', DeleteStoreProductAPIView.as_view(), name = 'delete-store-product'),
-    path('delete-employee/<str:id_employee>/', DeleteEmployeeAPIView.as_view(), name = 'delete-employee'),
-    path('delete-customer/<str:card_number>/', DeleteCustomerAPIView.as_view(), name = 'delete-customer'),
-    path('delete-check', DeleteCheckAPIView.as_view(), name = 'delete-check')
+    path('delete-product/<int:id_product>/', DeleteProductAPIView.as_view(), name='delete-product'),
+    path('delete-store-product/<str:UPC>/', DeleteStoreProductAPIView.as_view(), name='delete-store-product'),
+    path('delete-employee/<str:id_employee>/', DeleteEmployeeAPIView.as_view(), name='delete-employee'),
+    path('delete-customer/<str:card_number>/', DeleteCustomerAPIView.as_view(), name='delete-customer'),
+    path('delete-check', DeleteCheckAPIView.as_view(), name = 'delete-check'),
+
+    # urls for the report
+    path('statistics/', StatisticsAPIView.as_view(), name='statistics'),
+    path('categories-summary/', CategoriesSummaryAPIView.as_view(), name='categories-revenue'), # Evelina
+    path('sold-every-product/', SoldEveryProductAPIView.as_view(), name='sold-every-product'), # Evelina
+    path('category-average-price/', CategoryAveragePrice.as_view(), name='category-average-price'), # Andrii
+    path('all-products-are-sold/', AllProductsAreSold.as_view(), name='all-products-are-sold'), # Andrii
+    path('all-categories/', AllCategories.as_view(), name='all-categories'), # Damian
+    path('category-products-info/', CategoryProductInfo.as_view(), name='category-products-info'), # Damian
 ]
